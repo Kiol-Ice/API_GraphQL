ca3d6080f7dfc9ef19e45affc401304da9f10166
Kiol-Ice <ramadiel2001@gmail.com>
2024-03-07 12:29:44 +0100

    correction initialisation donnée

--- src/main/java/com/loick/graphql/api_graphql/Graphqlconfiguration.java
+++ src/main/java/com/loick/graphql/api_graphql/Graphqlconfiguration.java
@@ -2,6 +2,7 @@ package com.loick.graphql.api_graphql;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,31 +14,32 @@ import com.loick.graphql.api_graphql.entite.Team;
 
 @Configuration
 public class Graphqlconfiguration {
-    private List<Team> initTeams() {
+    @Bean
+    public TeamDao teamDao() {
+        List<Player> players = initPlayer();
+
         String[] teamName = {"Paris SG" , "AS Saint Etienne", "Olympique Lyonnais", "Olympique Marseillais", "FC Barcelone", "Real Madrid", "Manchester City", "Borussia Dortmund", "FC Bayern Munich", "FC Girondins de Bordeaux"};
         String[] teamLevel = {"Internationnal", "nationnal", "nationnal", "nationnal", "internationnal", "internationnal", "internationnal","internationnal","internationnal","nationnal" };
         String[] teamColor = {"Dark Blue", "Green and White", "Blue", "Light Blue and white", "Blue and Red", "White and gold", "Light Blue", "Yellow", "White and Red", "Dark Blue"};
         List<Team> teams = new ArrayList<>();
-        for (int playerId = 0; playerId < 10; ++playerId) {
+        for (int TeamId = 0; TeamId < 10; ++TeamId) {
             Team team = new Team();
-            team.setId("" + playerId);
-            team.setName(teamName[playerId]);
-            team.setLeagueLevel(teamLevel[playerId]);
-            team.setColor(teamColor[playerId]);
-            team.setPlayerId(null);
+            team.setId("" + TeamId);
+            team.setName(teamName[TeamId]);
+            team.setLeagueLevel(teamLevel[TeamId]);
+            team.setColor(teamColor[TeamId]);
+            team.setPlayerId(players.stream().filter(player -> player.getTeamId().equals(team.getId())).map(player->player.getId()).collect(Collectors.toList()));
             teams.add(team);
         }
-
-        return teams;
-    }
-
-    @Bean
-    public TeamDao teamDao() {
-        return new TeamDao(initTeams());
+        return new TeamDao(teams);
     }	
 
     @Bean
     public PlayerDao playerDao() {
+        return new PlayerDao(initPlayer());
+    }
+
+    private List<Player> initPlayer() {
         String[] playerFirstName = {"Kylian" , "Achraf", "Étienne", "Mathieu", "Robert", "Sergi", "Erling", "Kevin", "Harry", "Manuel"};
         String[] playerLastName = {"Mbappé", "Hakimi", "Green", "Cafaro", "Lewandowski", "Roberto", "Haaland","De Bruyne","Kane","Neuer" };
         String[] playerPosition = {"forward", "defender", "GoalKeeper", "midfielder", "forward", "defender", "forward", "midfielder", "forward", "goalkeeper"};
@@ -52,6 +54,6 @@ public class Graphqlconfiguration {
             player.setTeamId(playerTeam[playerId]);
             players.add(player);
         }
-        return new PlayerDao(players);
-    }	
+        return players;
+    }
 }
